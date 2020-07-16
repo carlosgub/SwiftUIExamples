aee4bde69f825fc3d27fc4e12d7fd9f68df9dbbf
Carlos Ugaz <carlosgabriel@carloss-MacBook-Pro.local>
2020-07-16 12:03:01 -0500

    first example

---
+++ RGBullsEye/RGBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by carlos gabriel on 7/15/20.
+//  Copyright © 2020 carlos gabriel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
